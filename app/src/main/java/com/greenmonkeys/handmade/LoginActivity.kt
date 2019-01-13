dc6d9214fc4fe80532be01613e08ce87f9d60cb3
Maxwell Ray <me@maxray.me>
2019-01-13 13:31:15 -0800

    Addition of Register page skeleton

--- app/src/main/java/com/greenmonkeys/handmade/LoginActivity.kt
+++ app/src/main/java/com/greenmonkeys/handmade/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.greenmonkeys.handmade
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onRegisterClick(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java).apply {}
+        startActivity(intent)
     }
 }
