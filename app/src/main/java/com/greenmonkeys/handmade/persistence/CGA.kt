4d4f1863f54a8f4783f5f60bfe1a9394e6a7663a
John Torres <jandrewtorres5@gmail.com>
2019-01-24 08:09:47 -0800

    Optimized imports. Removed unused Main activity. Added LWA workflow activity to Manifest. Added LWA workflow to Login activity. Added a few TODOs in Login.

--- app/src/main/java/com/greenmonkeys/handmade/persistence/CGA.kt
+++ app/src/main/java/com/greenmonkeys/handmade/persistence/CGA.kt
@@ -1,6 +1,5 @@
 package com.greenmonkeys.handmade.persistence
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
--- app/src/main/java/com/greenmonkeys/handmade/persistence/CGADao.kt
+++ app/src/main/java/com/greenmonkeys/handmade/persistence/CGADao.kt
@@ -1,6 +1,5 @@
 package com.greenmonkeys.handmade.persistence
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
